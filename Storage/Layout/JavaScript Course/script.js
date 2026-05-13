e801893973634066db4812a122f8958c67636945
Slime <irregularslime223@gmail.com>
2026-05-13 02:56:08 -0230

    Initial commit

---
+++ Storage/Layout/JavaScript Course/script.js
@@ -0,0 +1,8 @@
+let heading = document.getElementById("p1");
+heading.textContent = "New Heading";
+
+let paragraph = document.getElementById("p1");
+paragraph.style.color = "red";
+
+let button = document.getElementById("b1");
+button.style.border = solid;
