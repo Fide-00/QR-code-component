ac78f386ed12234de3a877dd53a9a5a2e4b59695
Fide-00 <irregularslime223@gmail.com>
2026-05-13 03:25:47 -0230

    Remove folders from repo but keep locally

--- Storage/Layout/JavaScript Course/script.js
+++
@@ -1,8 +0,0 @@
-let heading = document.getElementById("p1");
-heading.textContent = "New Heading";
-
-let paragraph = document.getElementById("p1");
-paragraph.style.color = "red";
-
-let button = document.getElementById("b1");
-button.style.border = solid;
